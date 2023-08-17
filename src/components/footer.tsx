7e2892922491797bd2fba301ef3d27790163c419
Donovan Glover <donovan@dglover.co>
2023-08-17 13:40:04 -0400

    components: Add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const Footer = () => {
+  return (
+    <footer className="site-footer">
+      <p>&copy; 2023 Donovan Glover</p>
+    </footer>
+  )
+}
+
+export default Footer
