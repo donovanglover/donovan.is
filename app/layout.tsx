e68065414e82aeeb440748f72198493bcfa65de0
Donovan Glover <donovan@dglover.co>
2023-09-05 16:24:29 -0400

    chore: Simplify main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,32 @@
 import '@/styles/main.sass'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog',
-  description: 'Next.js 13 App Router Blog',
+  title: "Donovan Glover",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en-us'>
-      <body className={inter.className}>
-        <div className='container'>
-          <header className='header'>
-            <Link href='/'>
-              <Image priority src='/images/profile.jpg' height={108} width={108} alt='' />
-            </Link>
-            <h1>
-              <Link href='/'>Donovan Glover</Link>
-            </h1>
-          </header>
+    <html lang="en-us">
+      <body>
+        <div className="top"></div>
+        <header>
+          <Link href="/">
+            <Image priority src="/images/profile.jpg" height={108} width={108} alt=""/>
+          </Link>
+          <h1><Link href="/">Donovan Glover</Link></h1>
+          <nav>
+            <Link href="/">Blog</Link> | <Link href="/projects">Projects</Link> | <Link href="/contact">Contact</Link>
+          </nav>
+        </header>
 
-          <main>{children}</main>
-          <Link href='/'>← Back to home</Link>
-        </div>
+        <main>{children}</main>
+
+        <footer>
+          <p>&copy; 2012-2023 Donovan Glover</p>
+          <p>Change Theme:</p>
+        </footer>
       </body>
     </html>
   )
