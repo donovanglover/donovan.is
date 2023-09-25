d8408b237ad261021737cf01a778d3b4a5133cbd
Donovan Glover <donovan@dglover.co>
2023-09-25 10:43:17 -0400

    feat: Add flake for nix develop

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Build Donovan's website";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          zola
+          bun
+          biome
+          fish
+        ];
+      };
+    });
+}
