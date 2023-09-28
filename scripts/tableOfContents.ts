6e73f37fb31d98cf3b4b9b843a894f1855cb73a6
Donovan Glover <donovan@dglover.co>
2023-09-28 17:57:57 -0400

    feat: Highlight headings when visiting links with hashes
    Visual indicator that makes it easier to see which heading one is
    supposed to be looking at.

--- scripts/tableOfContents.ts
+++ scripts/tableOfContents.ts
@@ -1,9 +1,42 @@
-export default function tableOfContents() {
+function removeExistingHighlight() {
+  const highlighted = document.getElementsByClassName("toc-highlight")
+
+  for (const element of highlighted) {
+    element.classList.remove("toc-highlight")
+  }
+}
+
+function highlightHeading(id?: string) {
+  removeExistingHighlight()
+
+  if (!id && !window.location.hash) return
+
+  document.getElementById(id ?? window.location.hash.substring(1))?.classList.add("toc-highlight")
+}
+
+function closeOnClick() {
   const toggle: HTMLInputElement = document.getElementById("toc-toggle") as HTMLInputElement
 
   document.querySelectorAll(".toc-container a").forEach(link => {
     link.addEventListener("click", () => {
       toggle.checked = false
+      highlightHeading()
     })
   })
+}
+
+function handleAnchorClicks() {
+  const links: NodeListOf<HTMLAnchorElement> = document.querySelectorAll(".header-anchor")
+
+  links.forEach(link => {
+    link.addEventListener("click", () => {
+      highlightHeading(link.hash.substring(1))
+    })
+  })
+}
+
+export default function tableOfContents() {
+  highlightHeading()
+  handleAnchorClicks()
+  closeOnClick()
 }
