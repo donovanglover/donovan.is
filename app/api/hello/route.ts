54fa992d28c1961fefd7a9a13d5724a2595219bb
Donovan Glover <donovan@dglover.co>
2024-01-19 18:28:29 -0500

    feat: Add github api route

--- app/api/hello/route.ts
+++
@@ -1,7 +0,0 @@
-// Next.js Edge API Route Handlers: https://nextjs.org/docs/app/building-your-application/routing/router-handlers#edge-and-nodejs-runtimes
-
-export const runtime = 'edge'
-
-export async function GET (): Promise<Response> {
-  return new Response(JSON.stringify({ name: 'John Doe' }))
-}
