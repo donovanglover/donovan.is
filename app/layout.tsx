dc5bf136967a3e163f12b8c1655fb1ab41f44c22
Donovan Glover <donovan@dglover.co>
2024-02-06 17:32:08 -0500

    feat: Add progress bar on page change
    Makes the website not hang indefinitely with no indicator if the user
    loses internet connection.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import { mapleMono } from '@/lib/fonts'
 import '@/styles/main.sass'
 import clsx from 'clsx'
-import Link from 'next/link'
 import { siteMetadata } from '@/lib/metadata'
 import Navbar from '@/components/Navbar'
+import HolyLoader from 'holy-loader'
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../tailwind.config'
+import Footer from '@/components/Footer'
+
+const fullConfig = resolveConfig(tailwindConfig)
 
 export const metadata = siteMetadata
 
@@ -15,8 +20,11 @@ export default function RootLayout ({ children }: RootLayoutProps): React.ReactE
   return (
     <html lang="en-US" className={clsx('text-white-400 bg-black-400', mapleMono.variable)}>
       <body>
+        <HolyLoader color={fullConfig.theme.colors.blue} />
         <Navbar />
+        <div className="pb-8" />
         {children}
+        <Footer />
       </body>
     </html>
   )
