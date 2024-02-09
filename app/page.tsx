7a184247265dce1b7e1cd98c5fc8c971639b8dd6
Donovan Glover <donovan@dglover.co>
2024-02-09 10:43:53 -0500

    feat: Show posts/projects on homepage
    Might show the projects in a different way or drop them entirely from
    the homepage later.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
-export const runtime = 'edge'
+import { getPosts, getProjects } from '@/lib/posts'
+import Link from 'next/link'
 
 export default async function HomePage (): Promise<React.ReactElement> {
+  const posts = await getPosts()
+  const projects = await getProjects()
+
   return (
-    <main>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error consequuntur incidunt alias corrupti magnam eius odit officia reprehenderit tempore, quae ullam, ea fugit cupiditate, culpa nobis voluptatum obcaecati veritatis ex.</p>
+    <main className="prose dark:prose-invert mx-auto container">
+      <h2>Writing</h2>
+      {posts.map(post => {
+        return (
+          <p key={post.slug}>
+            <span className="flex justify-between">
+              <Link href={`/${post.slug}`}>{post.title}</Link>
+              <span>{post.date.toISOString().slice(0, 10)}</span>
+            </span>
+          </p>
+        )
+      })}
+      <h2>Making</h2>
+      {projects.map(project => {
+        return (
+          <p key={project.slug}>
+            <Link href={`/${project.slug}`}>{project.title}</Link>
+          </p>
+        )
+      })}
     </main>
   )
 }
