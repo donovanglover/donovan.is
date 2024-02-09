d5dd6e3311d5facee84e56e6297324d4d05e4800
Donovan Glover <donovan@dglover.co>
2024-02-08 23:08:54 -0500

    components(NavLink): Simplify

--- components/client/NavLink.tsx
+++ components/client/NavLink.tsx
@@ -4,26 +4,18 @@ import clsx from 'clsx'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-export default function NavLink (
-  {
-    href,
-    title,
-    className
-  }:
-  {
-    href: string
-    title: string
-    className?: string
-  }
-): React.ReactElement {
+export interface NavLinkProps {
+  href: string
+  title: string
+  className?: string
+}
+
+export default function NavLink ({ href, title, className }: NavLinkProps): React.ReactElement {
   const pathname = usePathname()
 
   return (
     <li className={clsx('flex', className)}>
-      <Link href={href} className={clsx(
-        'p-4 hover:text-blue',
-        pathname === href && 'text-blue'
-      )}>{title}</Link>
+      <Link href={href} className={clsx('p-4 hover:text-blue-300', pathname === href && 'text-red-800')}>{title}</Link>
     </li>
   )
 }
