d1507ed1aa1530135773133c3bec5fdd8a396967
Donovan Glover <donovan@dglover.co>
2024-02-09 15:52:30 -0500

    chore: Add about page

---
+++ app/about/layout.tsx
@@ -0,0 +1,13 @@
+export interface AboutLayoutProps {
+  children: React.ReactNode
+}
+
+export default function AboutLayout ({ children }: AboutLayoutProps): React.ReactElement {
+  return (
+    <main>
+      <div className="prose dark:prose-invert p-4 mx-auto prose-lg xl:prose-xl font-serif prose-p:transition-colors prose-headings:transition-colors prose-a:transition-colors">
+        {children}
+      </div>
+    </main>
+  )
+}
