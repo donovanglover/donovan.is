6a833beb6212ffeccba863e7fd284c3723a1c6f2
Donovan Glover <donovan@dglover.co>
2024-02-12 01:47:19 -0500

    feat: Only pad y for prose container

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -5,7 +5,7 @@ export interface AboutLayoutProps {
 export default function AboutLayout ({ children }: AboutLayoutProps): React.ReactElement {
   return (
     <main>
-      <div className="prose dark:prose-invert p-4 mx-auto prose-lg xl:prose-xl font-serif prose-p:transition-colors prose-headings:transition-colors prose-a:transition-colors">
+      <div className="prose dark:prose-invert py-4 mx-auto prose-lg xl:prose-xl font-serif prose-p:transition-colors prose-headings:transition-colors prose-a:transition-colors">
         {children}
       </div>
     </main>
