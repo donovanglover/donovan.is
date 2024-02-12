baa8af6c2fb17353ed1092c41a31a3986f9f3323
Donovan Glover <donovan@dglover.co>
2024-02-11 23:58:03 -0500

    meta: Prefer holy-loader over loading.tsx
    Might change when skeletons are implemented.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { mapleMono } from '@/lib/fonts'
 import { meta, siteMetadata } from '@/lib/metadata'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import HolyLoader from 'holy-loader'
 
 export const metadata = siteMetadata
 
@@ -17,6 +18,7 @@ export default function RootLayout ({ children }: RootLayoutProps): React.ReactE
     <html lang={meta.lang} className={clsx(mapleMono.variable)}>
       <body className="text-[#222226] bg-slate-50 dark:bg-[#1f1f1f] dark:text-[#D4D4D4] transition-colors ease-in-out duration-500 flex flex-col h-screen text-pretty">
         <Navbar />
+        <HolyLoader />
         <div className="px-4 py-8 mx-auto container grow">
           {children}
         </div>
--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { FaSpinner } from 'react-icons/fa'
-
-export default function RootLoading (): React.ReactElement {
-  return (
-    <div className="py-4">
-      <FaSpinner className="animate-spin mx-auto text-4xl" />
-    </div>
-  )
-}
