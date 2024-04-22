f820e8647676f8e2da8a364a617e2476e4530b09
Donovan Glover <donovan@dglover.co>
2024-04-22 12:49:07 -0400

    chore: Make cards look nicer with opacity

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,7 +9,7 @@ export interface CardProps {
 
 export default function Card ({ href, className, children }: CardProps): React.ReactElement {
   return (
-    <Link href={href} className={clsx('m-2 flex flex-col justify-between border border-700 shadow hover:bg-200 hover:transition-colors md:m-4 xl:m-6', className)}>
+    <Link href={href} className={clsx('m-2 flex flex-col justify-between border border-700/10 shadow hover:bg-200/80 hover:transition-colors md:m-4 xl:m-6', className)}>
       {children}
     </Link>
   )
