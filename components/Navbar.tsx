21ddec35e5401017c472bc0cbe4bb455a65d04bf
Donovan Glover <donovan@dglover.co>
2024-04-23 17:46:57 -0400

    navbar: Remove home link
    Users were getting confused that clicking the name and the home link did
    the same thing, so I removed it.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,6 @@ export default function Navbar (): React.ReactElement {
       <nav className="flex justify-between">
         <NavLink href="/" title={meta.title} className="flex font-bold" active={false} />
         <ul className="flex">
-          <NavLink href="/" title="Home" />
           <NavLink href="/work" title="Work" />
           <NavLink href="/projects" title="Projects" />
           <NavLink href="/about" title="About" />
