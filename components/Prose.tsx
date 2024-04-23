ccb415e6aa434df49174179a46efbda7bb47f8bd
Donovan Glover <donovan@dglover.co>
2024-04-23 14:06:49 -0400

    prose: Transition <strong> colors

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -4,7 +4,7 @@ export interface ProseProps {
 
 export default function Prose ({ children }: ProseProps): React.ReactElement {
   return (
-    <article className="prose prose-lg mx-auto py-4 font-serif xl:prose-xl prose-headings:font-sans prose-headings:transition-colors prose-p:transition-colors prose-a:transition-colors hover:prose-a:text-orange">
+    <article className="prose prose-lg mx-auto py-4 font-serif xl:prose-xl prose-headings:font-sans prose-headings:transition-colors prose-p:transition-colors prose-a:transition-colors hover:prose-a:text-orange prose-strong:transition-colors">
       {children}
     </article>
   )
