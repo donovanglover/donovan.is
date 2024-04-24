6a22ba6d48c03b5c628f8d12ad676bcceffc1e49
Donovan Glover <donovan@dglover.co>
2024-04-24 10:15:55 -0400

    feat: Add initial support for various schemes
    Note that we might customize this list in the future to remove schemes
    that produce "bad" results.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,10 +2,12 @@
 
 import { meta } from '@/lib/metadata'
 import Anchor from './Anchor'
+import ChangeTheme from './client/ChangeTheme'
 
 export default function Footer (): React.ReactElement {
   return (
     <footer className="container mx-auto p-4 pt-0">
+      <ChangeTheme />
       <p className="m-2 text-center">&copy; 2024 <Anchor href={meta.social.mastodon}>Donovan Glover</Anchor> &middot; <Anchor href='https://github.com/donovanglover/donovan.is'>View source on GitHub</Anchor></p>
     </footer>
   )
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,5 @@
 import NavLink from '@/components/client/NavLink'
 import { meta } from '@/lib/metadata'
-import ChangeTheme from './client/ChangeTheme'
 
 export default function Navbar (): React.ReactElement {
   return (
@@ -11,7 +10,6 @@ export default function Navbar (): React.ReactElement {
           <NavLink href="/work" title="Work" />
           <NavLink href="/projects" title="Projects" />
           <NavLink href="/about" title="About" />
-          <li><ChangeTheme /></li>
         </ul>
       </nav>
     </header>
