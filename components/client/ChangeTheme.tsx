0b7149856926e45acec9acf173a18f3f60d6b2a3
Donovan Glover <donovan@dglover.co>
2024-04-24 12:58:53 -0400

    components(ChangeTheme): Add label for accessibility

--- components/client/ChangeTheme.tsx
+++ components/client/ChangeTheme.tsx
@@ -253,10 +253,13 @@ export default function ChangeTheme (): React.ReactElement {
   }
 
   return (
-    <select id="base16Scheme" value={currentScheme} onChange={changeScheme} className="mx-auto mb-4 block border border-200 bg-100 p-2 text-center text-700 shadow">
-      {schemes.map(scheme => {
-        return <option key={scheme} value={scheme}>{makeTitle(scheme.replace('base16-', ''))}</option>
-      })}
-    </select>
+    <div className="text-center">
+      <label htmlFor="base16Scheme">Change Theme:</label>
+      <select name="base16Scheme" id="base16Scheme" value={currentScheme} onChange={changeScheme} className="mb-2 ml-2 border border-200 bg-100 p-2 text-700 shadow">
+        {schemes.map(scheme => {
+          return <option key={scheme} value={scheme}>{makeTitle(scheme.replace('base16-', ''))}</option>
+        })}
+      </select>
+    </div>
   )
 }
