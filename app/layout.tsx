61ffa25e60c1f0c62bccd1480289528261b79979
Donovan Glover <donovan@dglover.co>
2024-04-24 21:32:35 -0400

    chore: Move fonts back to _fonts
    Less clutter in the root directory.

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,32 +24,32 @@ export const metadata = {
 const mapleMono = localFont({
   src: [
     {
-      path: '../assets/fonts/MapleMono-Light.woff2',
+      path: './_fonts/MapleMono-Light.woff2',
       weight: '300',
       style: 'normal'
     },
     {
-      path: '../assets/fonts/MapleMono-LightItalic.woff2',
+      path: './_fonts/MapleMono-LightItalic.woff2',
       weight: '300',
       style: 'italic'
     },
     {
-      path: '../assets/fonts/MapleMono-Regular.woff2',
+      path: './_fonts/MapleMono-Regular.woff2',
       weight: '400',
       style: 'normal'
     },
     {
-      path: '../assets/fonts/MapleMono-Italic.woff2',
+      path: './_fonts/MapleMono-Italic.woff2',
       weight: '400',
       style: 'italic'
     },
     {
-      path: '../assets/fonts/MapleMono-Bold.woff2',
+      path: './_fonts/MapleMono-Bold.woff2',
       weight: '700',
       style: 'normal'
     },
     {
-      path: '../assets/fonts/MapleMono-BoldItalic.woff2',
+      path: './_fonts/MapleMono-BoldItalic.woff2',
       weight: '700',
       style: 'italic'
     }
