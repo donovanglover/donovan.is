7396919ae5991d3ac803b6929f6afa4dcb9ed7d6
Donovan Glover <donovan@dglover.co>
2024-04-27 19:25:24 -0400

    chore: Improve 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,19 @@
+import { type Metadata } from 'next'
+import Link from 'next/link'
+import Prose from '@/components/Prose'
+
+export const metadata: Metadata = {
+  title: '404 Not Found',
+  description: "There's nothing here. Try going to the home page."
+}
+
 export default function NotFound (): React.ReactElement {
   return (
-    <>
-      <p>404</p>
-    </>
+    <main>
+      <Prose>
+        <h1>404 Not Found</h1>
+        <p className="date">There&apos;s nothing here. Try going to the <Link href="/">home page</Link>.</p>
+      </Prose>
+    </main>
   )
 }
