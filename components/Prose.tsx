cdfc7d8558b55e7a61c977fcf396b986110ec01f
Donovan Glover <donovan@dglover.co>
2024-05-02 17:05:03 -0400

    fix: Add missing transition-colors for inline code

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -41,7 +41,7 @@ export default function Prose ({ children }: ProseProps): React.ReactElement {
   })
 
   return (
-    <article className="prose prose-lg mx-auto py-4 font-serif xl:prose-xl prose-headings:font-sans prose-headings:transition-colors prose-p:transition-colors prose-a:transition-colors hover:prose-a:text-orange prose-strong:transition-colors">
+    <article className="prose prose-lg mx-auto py-4 font-serif xl:prose-xl prose-headings:font-sans prose-headings:transition-colors prose-p:transition-colors prose-a:transition-colors hover:prose-a:text-orange prose-strong:transition-colors prose-code:transition-colors">
       {children}
     </article>
   )
