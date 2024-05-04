d453a5cb79091135aa2256fc03d14e9ea4a09985
Donovan Glover <donovan@dglover.co>
2024-05-04 14:46:45 -0400

    feat: Use single theme-color for both light and dark mode
    Some browsers don't seem to handle prefers-color-scheme properly. This
    should fix things.

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,7 @@ import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 
 export const viewport: Viewport = {
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
-    { media: '(prefers-color-scheme: dark)', color: '#181818' }
-  ]
+  themeColor: '#181818'
 }
 
 export const metadata: Metadata = {
