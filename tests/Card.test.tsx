76e9c74a951d298fa76f9f307a238581c67e8c12
Donovan Glover <donovan@dglover.co>
2024-05-11 12:27:43 -0400

    feat: Use vitest for tests
    So far seems easier and more straight-forward than having to deal with
    the Storybook method for tests.

---
+++ tests/Card.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react'
+import { beforeEach, describe, expect, it } from 'vitest'
+import Card from '@/components/Card'
+
+describe('Card', () => {
+  beforeEach(() => {
+    render(
+      <Card
+        href='https://www.example.com'
+        project={{
+          name: 'new-tab-identity',
+          stars: 0,
+          pushed: 2024,
+          created: 2024,
+          language: 'typescript',
+          description: 'Browse the web from multiple locations at once with Mullvad VPN.'
+        }}
+      />
+    )
+  })
+
+  it('should render', () => {
+    expect(screen.getByText(/new-tab-identity/)).toBeDefined()
+  })
+})
